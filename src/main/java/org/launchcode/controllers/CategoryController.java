475a2722f3a8e3cb3aafd779068ccb0975174f9a
carlgillis <carldgillis@gmail.com>
2018-06-23 15:25:33 -0500

    All features for Part 1 added and working properly

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,5 +1,5 @@
 package org.launchcode.controllers;
-import org.launchcode.models.data.Category;
+import org.launchcode.models.Category;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.launchcode.models.data.CategoryDao;
 
-import javax.jws.WebParam;
 import javax.validation.Valid;
 
 
@@ -28,17 +27,16 @@ public class CategoryController {
         model.addAttribute("categories", categoryDao.findAll());
         model.addAttribute("title", "Categories");
 
-
         return "category/index";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
-    public String displayAddCategoryForm(Model model) {
-        model.addAttribute(new Category());
+    public String add(Model model) {
         model.addAttribute("title", "Add Category");
-
+        model.addAttribute(new Category());
         return "category/add";
     }
+
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String add(Model model, @ModelAttribute @Valid Category category, Errors errors) {
--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,11 +1,13 @@
-package org.launchcode.models.data;
-
+package org.launchcode.models;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+
+
 
 @Entity
 public class Category {
@@ -18,24 +20,18 @@ public class Category {
     @Size(min=3, max=15)
     private String name;
 
+    public Category(String name) { this.name = name; }
+
+    public Category() { }
+
     public int getId() {
         return id;
     }
 
-    public String getName() {
+    public String getName() { return name; }
 
-        return name;
-    }
+    public void setName(String name) { this.name = name; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public Category() {
 
-    }
-
-    public Category(String name) {
-        this.name = name;
-    }
 }
